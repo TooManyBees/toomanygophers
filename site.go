57e4ee516bd637e646c7b4bda79280791f6aa5e3
Jesse Kipp <esbe.v2@gmail.com>
2014-10-04 18:24:16 -0400

    Adds whitelist for http methods

--- site.go
+++ site.go
@@ -16,18 +16,34 @@ func avatarHandler(w http.ResponseWriter, r *http.Request) {
   avatarTempl.ExecuteTemplate(w, "page", nil)
 }
 
-func loggingHandler(handler func(http.ResponseWriter, *http.Request) ) http.HandlerFunc {
+// Wraps an endpoint handler in logging statements, as well as
+// enforcing the allowed HTTP methods for it
+func loggingHandler(handler func(http.ResponseWriter, *http.Request), methods ...string) http.HandlerFunc {
+  includes := func(needle string, haystack ...string) bool {
+    for _, str := range haystack {
+      if str == needle {
+        return true
+      }
+    }
+    return false
+  }
+
   return func(w http.ResponseWriter, r *http.Request) {
     fmt.Printf("%s  %s", r.RemoteAddr, r.URL)
-    handler(w, r)
+    if len(methods) == 0 || includes(r.Method, methods...) {
+      handler(w, r)
+    } else {
+      fmt.Print("  unsupported method")
+      w.WriteHeader(http.StatusMethodNotAllowed)
+    }
     fmt.Print("\n")
   }
 }
 
 var pageCache = PageCache{}
 func main() {
-  http.HandleFunc("/", loggingHandler(indexHandler))
-  http.HandleFunc("/avatar", loggingHandler(avatarHandler))
+  http.HandleFunc("/", loggingHandler(indexHandler, "GET"))
+  http.HandleFunc("/avatar", loggingHandler(avatarHandler, "GET"))
   http.Handle("/s/", http.StripPrefix("/s/", http.FileServer(http.Dir("public"))))
   http.ListenAndServe(":8080", nil)
 }
